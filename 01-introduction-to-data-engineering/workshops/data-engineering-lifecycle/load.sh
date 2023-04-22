5179cf6794842589476c4508e3628c010c26e918
Wood <95164370+StableBamBoo@users.noreply.github.com>
2023-04-22 10:18:56 +0000

    workshop-day-1

--- 01-introduction-to-data-engineering/workshops/data-engineering-lifecycle/load.sh
+++ 01-introduction-to-data-engineering/workshops/data-engineering-lifecycle/load.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl -XPOST -H "Content-type: application/json" -d @dogs.json "https://getpantry.cloud/apiv1/pantry/<YOUR-PANTRY-ID>/basket/randomDogs"
+curl -XPOST -H "Content-type: application/json" -d @dogs.json "https://getpantry.cloud/apiv1/pantry/351e572c-7a51-4f2d-b16d-2dfc8ff67f48/basket/randomDogs"
